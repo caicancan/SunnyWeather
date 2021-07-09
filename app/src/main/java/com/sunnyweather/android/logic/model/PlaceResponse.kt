f6dd9961617b7e9520f50bf611ac6fb2d5de7e91
caicancan <18062411472@163.com>
2021-07-09 22:37:05 +0800

    commit -third

--- app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
@@ -1,4 +1,8 @@
 package com.sunnyweather.android.logic.model
 
-class PlaceResponse {
-}
+import android.location.Location
+import com.google.gson.annotations.SerializedName
+
+data class PlaceResponse(val status:String,val location : Location,@SerializedName("formatted_address")
+val address:String)
+data class Location(val lng:String,val lat:String)
